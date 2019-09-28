8926fa6486a747c4442ff62930bb208c5bf7690f
KhanhDX <khanhdx@fsoft.com.vn>
2019-09-28 12:29:05 +0700

    update db

--- db.sql
+++ db.sql
@@ -13,12 +13,38 @@
 
 
 -- Dumping database structure for final_project
-DROP DATABASE IF EXISTS `final_project`;
 CREATE DATABASE IF NOT EXISTS `final_project` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `final_project`;
 
+-- Dumping structure for table final_project.app_role
+CREATE TABLE IF NOT EXISTS `app_role` (
+  `ROLE_ID` bigint(20) NOT NULL,
+  `ROLE_NAME` varchar(30) NOT NULL,
+  PRIMARY KEY (`ROLE_ID`),
+  UNIQUE KEY `APP_ROLE_UK` (`ROLE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table final_project.app_role: ~0 rows (approximately)
+DELETE FROM `app_role`;
+/*!40000 ALTER TABLE `app_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app_role` ENABLE KEYS */;
+
+-- Dumping structure for table final_project.app_user
+CREATE TABLE IF NOT EXISTS `app_user` (
+  `USER_ID` bigint(20) NOT NULL,
+  `USER_NAME` varchar(36) NOT NULL,
+  `ENCRYTED_PASSWORD` varchar(128) NOT NULL,
+  `ENABLED` bit(1) NOT NULL,
+  PRIMARY KEY (`USER_ID`),
+  UNIQUE KEY `APP_USER_UK` (`USER_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table final_project.app_user: ~0 rows (approximately)
+DELETE FROM `app_user`;
+/*!40000 ALTER TABLE `app_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app_user` ENABLE KEYS */;
+
 -- Dumping structure for table final_project.entities
-DROP TABLE IF EXISTS `entities`;
 CREATE TABLE IF NOT EXISTS `entities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -31,7 +57,6 @@ DELETE FROM `entities`;
 /*!40000 ALTER TABLE `entities` ENABLE KEYS */;
 
 -- Dumping structure for table final_project.hibernate_sequence
-DROP TABLE IF EXISTS `hibernate_sequence`;
 CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
   `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -43,10 +68,23 @@ INSERT INTO `hibernate_sequence` (`next_val`) VALUES
 	(14);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 
+-- Dumping structure for table final_project.persistent_logins
+CREATE TABLE IF NOT EXISTS `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table final_project.persistent_logins: ~0 rows (approximately)
+DELETE FROM `persistent_logins`;
+/*!40000 ALTER TABLE `persistent_logins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `persistent_logins` ENABLE KEYS */;
+
 -- Dumping structure for table final_project.teacher
-DROP TABLE IF EXISTS `teacher`;
 CREATE TABLE IF NOT EXISTS `teacher` (
-  `teacher_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `teacher_id` int(11) NOT NULL AUTO_INCREMENT,
   `teacher_name` varchar(45) DEFAULT NULL,
   `teacher_age` int(11) DEFAULT NULL,
   `teacher_gender` varchar(10) DEFAULT NULL,
@@ -55,16 +93,29 @@ CREATE TABLE IF NOT EXISTS `teacher` (
   `teacher_phone` varchar(10) DEFAULT NULL,
   `status` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`teacher_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table final_project.teacher: ~3 rows (approximately)
+-- Dumping data for table final_project.teacher: ~0 rows (approximately)
 DELETE FROM `teacher`;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` (`teacher_id`, `teacher_name`, `teacher_age`, `teacher_gender`, `teacher_address`, `teacher_email`, `teacher_phone`, `status`) VALUES
-	(11, 'KhanhDX', 21, 'Male', 'Hanoi', 'khanhdx@gmail.com', '0398787522', 1),
-	(12, 'ThuyetLV', 28, 'Male', 'Hanoi', 'thuyetlv@gmail.com', '0398787522', 1),
-	(13, 'TruongNV', 27, 'Male', 'Hanoi', 'truongnv@gmail.com', '0398787522', 1);
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+
+-- Dumping structure for table final_project.user_role
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `ID` bigint(20) NOT NULL,
+  `USER_ID` bigint(20) NOT NULL,
+  `ROLE_ID` bigint(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `USER_ROLE_UK` (`USER_ID`,`ROLE_ID`),
+  KEY `USER_ROLE_FK2` (`ROLE_ID`),
+  CONSTRAINT `USER_ROLE_FK1` FOREIGN KEY (`USER_ID`) REFERENCES `app_user` (`USER_ID`),
+  CONSTRAINT `USER_ROLE_FK2` FOREIGN KEY (`ROLE_ID`) REFERENCES `app_role` (`ROLE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table final_project.user_role: ~0 rows (approximately)
+DELETE FROM `user_role`;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
